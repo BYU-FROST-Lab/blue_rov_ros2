97c04af72f57f646f7dc57542f90aabce9b45e47
Braden Meyers <bjz-github@spwiz.com>
2026-01-08 12:19:16 -0700

    Packages update for this repo

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,12 @@
 # !/bin/bash
 
-# TODO improve this for removing and re pulling with branches
+set -e
+cd "$(dirname "$0")/.." 
+mkdir -p packages   
 
+# TODO improve this for removing and re pulling with branches
 # VCS import to update all repos
 vcs import packages < .vcs/ros2_packages
+
+cd packages/dvl-a50
+git submodule update --init --recursive
