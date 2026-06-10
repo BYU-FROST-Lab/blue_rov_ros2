4456794bc8cb9b61e239460833a0d5ab7dbc7f4c
Braden <bjz-github@spwiz.com>
2026-06-10 17:31:00 -0600

    rqt reconfigure adding to tmux base

--- base_scripts/tmux.sh
+++ base_scripts/tmux.sh
@@ -18,7 +18,7 @@ tmux set-option -t ${SESSION_NAME} default-terminal "screen-256color"
 ########################
 tmux rename-window -t ${SESSION_NAME}:0 base
 
-# Split into left/right (50/50)
+# Split into left/right 
 tmux split-window -h -t ${SESSION_NAME}:0
 
 # left pane
@@ -32,6 +32,7 @@ tmux send-keys -t ${SESSION_NAME}:0.1 \
 # Window 1: empty
 ########################
 tmux new-window -t ${SESSION_NAME} -n ros2
+tmux split-window -h -t ${SESSION_NAME}:ros2
 
 xhost + # Allow connections from any host (for X11 forwarding)
 tmux send-keys -t ${SESSION_NAME}:1.0 \
@@ -45,7 +46,14 @@ tmux send-keys -t ${SESSION_NAME}:1.0 \
 tmux send-keys -t ${SESSION_NAME}:1.0 \
   "ros2 launch sensor_bringup base_station_launch.py" 
 
-
+tmux send-keys -t ${SESSION_NAME}:1.1 \
+  "docker exec -it ${CONTAINER} bash" C-m
+tmux send-keys -t ${SESSION_NAME}:1.1 \
+  "source install/setup.bash" C-m
+tmux send-keys -t ${SESSION_NAME}:1.1 \
+  "clear" C-m
+tmux send-keys -t ${SESSION_NAME}:1.1 \
+  "ros2 rqt_reconfigure rqt_reconfigure" 
 
 # Select the first window
 tmux select-window -t ${SESSION_NAME}:0
